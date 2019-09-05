603f8a38821ebc912bcb592ea001319ef5b7cbf3
Modurotolu Olokode <durolawk@gmail.com>
2019-09-05 15:50:20 +0100

    my github card created

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,18 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+//function myFunc () {
+  axios.get ('https://api.github.com/users/durotolu')
+  .then(refs => {
+    //debugger
+    //cardMaker(res); // return div
+    const cardHolder = document.querySelector('.cards');
+    cardHolder.appendChild(cardMaker(refs));
+  })
+  .catch(error => {
+    //debugger
+  })
+//}
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -45,6 +57,50 @@ const followersArray = [];
 </div>
 
 */
+function cardMaker (response) {
+  //const {} = data;
+  const div = document.createElement('div');
+  div.setAttribute('class', 'card');
+  const img = document.createElement('img');
+  img.setAttribute('src', response.data.avatar_url);
+  const div2 = document.createElement('div');
+  div2.setAttribute('class', 'card-info');
+  const h3 = document.createElement('h3');
+  h3.setAttribute('class', 'name');
+  h3.textContent = response.data.name;
+  const p1 = document.createElement('p');
+  p1.setAttribute('class', 'username');
+  p1.textContent = response.data.login;
+  const p2 = document.createElement('p');
+  p2.textContent = 'Location: ' + response.data.location;
+  const p3 = document.createElement('p');
+  p3.textContent = "Profile: ";
+  const a = document.createElement('a');
+  a.setAttribute('href', response.data.url);
+  a.textContent = response.data.url;
+  const p4 = document.createElement('p');
+  p4.textContent = 'Followers: ' + response.data.followers_url;
+  const p5 = document.createElement('p');
+  p5.textContent = 'Following: ' + response.data.following_url;
+  const p6 = document.createElement('p');
+  p6.textContent = "Bio: " + response.data.bio;
+
+  div.appendChild(img);
+  div.appendChild(div2);
+  div2.appendChild(h3);
+  div2.appendChild(p1);
+  div2.appendChild(p2);
+  div2.appendChild(p3);
+  p3.appendChild(a);
+  div2.appendChild(p4);
+  div2.appendChild(p5);
+  div2.appendChild(p6);
+
+  return div;
+}
+
+// const cardHolder = document.querySelector('.cards');
+// cardHolder.appendChild(div);
 
 /* List of LS Instructors Github username's: 
   tetondan
